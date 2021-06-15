d43d1e74345c3afb0e6113d6668e2accc2a16336
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2021-06-15 18:46:13 +0100

    files

---
+++ script.js
@@ -0,0 +1,41 @@
+/* <h1>I'm Stephen Bardwell
+<? date_default_timezone_set('UTC');
+   if (date('d m') === '10 01') : ?>
+    <span class="text">and it is my birthday!</span></h1>
+<? else : ?>
+    <span>and <?= $quoteHandler->getQuote(); ?></span></h1>
+<? endif; ?> */
+
+var quotesArray = [
+  'I drive a hard bargain',
+  'I know a guy down the market',
+  'cinnamon is a spice',
+  'I can never find a good caesar salad',
+  'pizza is the worst food you can eat',
+  'bard bard bard bard bard bard',
+  'I am going to shoot some hoops',
+  'I am off for a lunchtime walk',
+  'I like my coffee strong',
+  'I\'ve got nothing left',
+  'do you want some better quality tea bags?',
+  'I know my films',
+  'stop eating my Fruit & Fibre!',
+  'stop buying me Fruit and Fibre!'
+]
+var text = 'I\'m Stephen Bardwell';
+
+function isBirthday() {
+  var bardsBirthday = new Date('1968-01-10');
+  var today = new Date();
+
+  return (
+    bardsBirthday.getDate() == today.getDate() &&
+    bardsBirthday.getMonth() == today.getMonth()
+  );
+}
+
+const quote = isBirthday() ? 'and it is my birthday!' : quotesArray[Math.floor(Math.random() * quotesArray.length)];
+
+var fullText = text + ' ' + quote;
+
+document.querySelector('.quote').textContent = fullText;
